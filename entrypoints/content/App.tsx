fdd1be5df6e706bd407637b7db7788dd38419054
brkunver <burakhanunver@gmail.com>
2025-04-24 12:56:36 +0300

    add content ui

---
+++ entrypoints/content/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="absolute right-10 bottom-10 z-50 w-fit">
+      <h1 className="rounded bg-black p-2 text-white">Hello from content</h1>
+    </div>
+  )
+}
