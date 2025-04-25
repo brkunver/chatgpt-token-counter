8c88dd63b68bfe2f967ab1d33195c23c8410d058
brkunver <burakhanunver@gmail.com>
2025-04-25 16:42:07 +0300

    set setting on install

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,26 @@
+import { extensionActiveStorage, updateIntervalStorage } from "@/utils/storage-helpers"
+import { DEFAULT_UPDATE_INTERVAL } from "@/constants"
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+  browser.runtime.onInstalled.addListener(details => {
+    if (details.reason === "install") {
+      extensionActiveStorage
+        .setValue(true)
+        .then(() => {
+          console.log("Extension activated")
+        })
+        .catch(error => {
+          console.error("Failed to activate extension:", error)
+        })
+
+      updateIntervalStorage
+        .setValue(DEFAULT_UPDATE_INTERVAL)
+        .then(() => {
+          console.log("Update interval set to ", DEFAULT_UPDATE_INTERVAL)
+        })
+        .catch(error => {
+          console.error("Failed to set update interval:", error)
+        })
+    }
+  })
+})
