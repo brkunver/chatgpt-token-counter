6cd579632befc1aff68e561bebd4ecb714793156
brkunver <burakhanunver@gmail.com>
2025-04-25 17:00:16 +0300

    add settings

--- entrypoints/content/App.tsx
+++ entrypoints/content/App.tsx
@@ -1,19 +1,49 @@
 import { useEffect, useState } from "react"
-import { pollChatMessages } from "@/utils/pollChatMessages"
-import { tokenCounter } from "@/utils/tokenCounter"
-import { wordCounter } from "@/utils/wordCounter"
+import { pollChatMessages } from "@/utils/poll-chat-messages"
+import { tokenCounter } from "@/utils/token-counter"
+import { wordCounter } from "@/utils/word-counter"
+import { extensionActiveStorage, updateIntervalStorage } from "@/utils/storage-helpers"
+
+import { DEFAULT_UPDATE_INTERVAL } from "@/constants"
 
 export default function App() {
   const [tokenCount, setTokenCount] = useState(10)
   const [characterCount, setCharacterCount] = useState(10)
+  const [isExtensionActive, setIsExtensionActive] = useState<boolean>(true)
+  const [countInterval, setCountInterval] = useState<number>(DEFAULT_UPDATE_INTERVAL)
+
+  console.log("Dev : ", countInterval)
 
   useEffect(() => {
-    pollChatMessages((userText, assistantText) => {
+    const intervalId = pollChatMessages((userText, assistantText) => {
       const allText = userText + " " + assistantText
       setTokenCount(tokenCounter(allText))
       setCharacterCount(wordCounter(allText))
-    })
+    }, countInterval)
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [countInterval])
+
+  useEffect(() => {
+    extensionActiveStorage.watch(value => setIsExtensionActive(value))
+    updateIntervalStorage.watch(value => setCountInterval(value))
+
+    async function InitActive() {
+      let isActive = await extensionActiveStorage.getValue()
+      setIsExtensionActive(isActive)
+    }
+
+    async function InitInterval() {
+      let interval = await updateIntervalStorage.getValue()
+      setCountInterval(interval)
+    }
+
+    InitActive()
+    InitInterval()
   }, [])
+
+  if (!isExtensionActive) return null
 
   return (
     <div className="fixed top-2 left-1/2 z-[9999] flex w-fit -translate-x-1/2 flex-col gap-2 rounded bg-[#121212] p-2 font-semibold text-white xl:top-24 xl:right-4 xl:left-auto xl:translate-x-0">
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,7 +1,62 @@
+import { extensionActiveStorage, updateIntervalStorage } from "@/utils/storage-helpers"
+
 function App() {
+  const [updateInterval, setUpdateInterval] = useState<number>(1000)
+  const [isExtensionActive, setIsExtensionActive] = useState<boolean>(true)
+
+  console.log(isExtensionActive, updateInterval)
+
+  useEffect(() => {
+    extensionActiveStorage.watch(value => setIsExtensionActive(value))
+    updateIntervalStorage.watch(value => setUpdateInterval(value))
+
+    async function InitActive() {
+      let isActive = await extensionActiveStorage.getValue()
+      setIsExtensionActive(isActive)
+    }
+
+    async function InitInterval() {
+      let interval = await updateIntervalStorage.getValue()
+      setUpdateInterval(interval)
+    }
+
+    InitActive()
+    InitInterval()
+  }, [])
+
+  function onSaveChanges() {
+    updateIntervalStorage.setValue(updateInterval)
+    extensionActiveStorage.setValue(isExtensionActive)
+  }
+
   return (
     <main className="flex min-w-[300px] flex-col p-4">
       <h1 className="text-2xl font-bold">Chatgpt Token Counter</h1>
+      <h2>Settings</h2>
+      <div className="flex">
+        <label htmlFor="extension-active">Extension Active:</label>
+        <input
+          type="checkbox"
+          id="extension-active"
+          checked={isExtensionActive}
+          onChange={() => setIsExtensionActive(!isExtensionActive)}
+        />
+      </div>
+      <div className="flex">
+        <label htmlFor="update-interval">Update Interval (ms):</label>
+        <input
+          type="number"
+          min={10}
+          max={10000}
+          step={1}
+          value={updateInterval}
+          id="update-interval"
+          onChange={e => setUpdateInterval(Number(e.target.value))}
+        />
+      </div>
+      <button className="cursor-pointer rounded px-2 py-1" onClick={onSaveChanges}>
+        Save Changes
+      </button>
     </main>
   )
 }
