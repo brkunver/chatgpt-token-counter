343e6ea5c83c8629a01eeb714e7ce7ec99097b41
brkunver <burakhanunver@gmail.com>
2025-05-02 14:51:19 +0300

    remove path check

--- entrypoints/content/App.tsx
+++ entrypoints/content/App.tsx
@@ -7,19 +7,26 @@ import { extensionActiveStorage, updateIntervalStorage } from "@/utils/storage-h
 import { DEFAULT_UPDATE_INTERVAL } from "@/utils/constants"
 
 export default function App() {
-  const [tokenCount, setTokenCount] = useState(10)
-  const [characterCount, setCharacterCount] = useState(10)
+  const [tokenCount, setTokenCount] = useState(0)
+  const [characterCount, setCharacterCount] = useState(0)
   const [isExtensionActive, setIsExtensionActive] = useState<boolean>(true)
   const [countInterval, setCountInterval] = useState<number>(DEFAULT_UPDATE_INTERVAL)
 
-  console.log("Dev : ", countInterval)
+  console.log("Dev : Token Counter Rendered")
 
   useEffect(() => {
-    const intervalId = pollChatMessages((userText, assistantText) => {
-      const allText = userText + " " + assistantText
-      setTokenCount(tokenCounter(allText))
-      setCharacterCount(wordCounter(allText))
-    }, countInterval)
+    let intervalId: NodeJS.Timeout
+
+    const startPolling = () => {
+      intervalId = pollChatMessages((userText, assistantText) => {
+        const allText = userText + " " + assistantText
+        setTokenCount(tokenCounter(allText))
+        setCharacterCount(wordCounter(allText))
+      }, countInterval)
+    }
+
+    startPolling()
+
     return () => {
       clearInterval(intervalId)
     }
@@ -45,13 +52,8 @@ export default function App() {
 
   if (!isExtensionActive) return null
 
-  // only render on conversation page
-  const path = typeof window !== "undefined" ? window.location.pathname : ""
-  const isConversationPage = /^\/c\/[\w-]+$/.test(path)
-  if (!isConversationPage) return null
-
   return (
-    <div className="font-main fixed top-2 left-1/2 z-[9999] flex w-fit -translate-x-1/2 gap-2 rounded bg-[#121212] p-2 text-sm text-white xl:top-24 xl:right-4 xl:left-auto xl:translate-x-0 xl:flex-col">
+    <div className="font-main fixed top-2 left-1/2 z-[999999] flex w-fit -translate-x-1/2 gap-2 rounded bg-[#121212] p-2 text-sm text-white xl:top-24 xl:right-4 xl:left-auto xl:translate-x-0 xl:flex-col">
       <p className="flex justify-between gap-2">
         <span>{tokenCount}</span>
         <span className="text-gray-400">Tokens</span>
