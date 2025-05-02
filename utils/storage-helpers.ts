df889ec2d65a278c7dab5bba9449e3c030d82632
brkunver <burakhanunver@gmail.com>
2025-05-02 12:00:52 +0300

    add default update interval fallback to storage

--- utils/storage-helpers.ts
+++ utils/storage-helpers.ts
@@ -1,11 +1,12 @@
 import { storage } from "#imports"
+import { DEFAULT_UPDATE_INTERVAL } from "@/utils/constants"
 
 let extensionActiveStorage = storage.defineItem<boolean>("local:extension-active", {
   fallback: true,
 })
 
 let updateIntervalStorage = storage.defineItem<number>("local:update-interval", {
-  fallback: 1000,
+  fallback: DEFAULT_UPDATE_INTERVAL,
 })
 
 export { extensionActiveStorage, updateIntervalStorage }
