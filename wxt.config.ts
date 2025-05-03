a3d30fa5f13801723b29d44fa3e5f5c46fdb1e68
brkunver <burakhanunver@gmail.com>
2025-05-03 14:22:39 +0300

    init i18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,11 @@ import Tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
   manifest: {
+    name: "__MSG_extName__",
+    description: "__MSG_extDescription__",
+    default_locale: "en",
     permissions: ["storage"],
     web_accessible_resources: [{ resources: ["fonts/*"], matches: ["*://chatgpt.com/*"] }],
   },
